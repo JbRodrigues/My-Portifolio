9368d7fc18491ccb4f08b8a6e36cc186f3cc5efa
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2023-02-09 15:55:16 -0300

    Fix: now the click function of the exp button works :)

--- src/script/code.js
+++ src/script/code.js
@@ -1,13 +1,30 @@
 const selected_buttons = document.querySelector('.exp-buttons');
 const btns = document.querySelectorAll('.btns');
 
+const soft = document.querySelector('.exp-soft');
+const zazz = document.querySelector('.exp-zazz');
+
 selected_buttons.addEventListener('click', e => {
 
     btns.forEach(btn => {
 
-        if (btn.getAttribute('id') === e.target.getAttribute('id'))
+        if (btn.getAttribute('id') === e.target.getAttribute('id')) {
+            alert(btn.getAttribute('id'))
             btn.classList.add('active');
-        else
+
+            if(btn.getAttribute('id') == 'zazz'){
+                soft.style.display = 'none'
+                zazz.style.display = 'block'
+            }else{
+                soft.style.display = 'block'
+                zazz.style.display = 'none'
+            }
+        } else {
             btn.classList.remove('active');
+        }
+
+        if (btn.getAttribute('id') == "exp-2") {
+            btn.getAttribute('id').style.display = "block"
+        }
     });
 });
